8bc36e1010a13b980903fc47b5b4007cc89e6e65
will <126467604+Ifwill@users.noreply.github.com>
2024-03-13 11:23:34 -0300

    Add files via upload

---
+++ cashFlow/js/home.js
@@ -0,0 +1,22 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
+
+// JavaScript para fechar o menu dropdown quando o usuário clicar fora dele
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
